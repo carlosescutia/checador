56fb0ee0acf04353703d73ff759b785e4e8730c6
Carlos Escutia <carlosescutia@gmail.com>
2023-10-19 11:16:07 -0600

    Se agregan indicadores de avance por empleado, avance por día y carga de informacion.

--- application/views/admin/incidentes_dia.php
+++ application/views/admin/incidentes_dia.php
@@ -3,21 +3,48 @@
         <strong>Por dia</strong>
     </div>
     <div class="card-body">
-        <div class="text-center" id="canvas-holder" style="width:100%">
-            <canvas id="avance_dias"></canvas>
-        </div>
-        <div class="col-md-12">
+        <div class="row">
+            <h5 class="text-center">Incidentes</h5>
             <div class="row">
-                <div class="col-md-6 text-center">
-                    <h5>Días con incidentes</h5>
-                    <h1><?= $tot_dias_incidentes ?></h1>
+                <div class="col-sm-10 offset-sm-1" id="canvas-holder">
+                    <canvas id="avance_dias"></canvas>
                 </div>
-                <div class="col-md-6 text-center">
-                    <h5>Días hábiles</h5>
-                    <h1><?= $tot_dias_habiles ?></h1>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <h6>Días con incidentes</h6>
+                        <h1><?= $tot_dias_incidentes ?></h1>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <h6>Días con información</h6>
+                        <h1><?= $tot_dias_info ?></h1>
+                    </div>
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <h5 class="text-center">Carga de información</h5>
+            <div class="row">
+                <div class="col-sm-10 offset-sm-1" id="canvas-holder">
+                    <canvas id="carga_info"></canvas>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <h6>Días con información</h6>
+                        <h1><?= $tot_dias_info ?></h1>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <h6>Días hábiles</h6>
+                        <h1><?= $tot_dias_habiles ?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
     </div>
 </div>
 
--- application/views/admin/indicadores_incidentes.php
+++
@@ -1,7 +0,0 @@
-<div class="card mt-0 mb-3 border-0">
-    <div class="card-body">
-        <h4><?= $tot_incidentes ?> incidentes totales</h4>
-        <h4><?= $tot_empleados_incidentes ?> empleados con incidentes</h4>
-        <h4><?= $tot_dias_incidentes ?> dias con incidentes</h4>
-    </div>
-</div>
