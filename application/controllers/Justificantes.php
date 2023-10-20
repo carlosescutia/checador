e547fdbeb124c9337f654c5929ca4118a7780ba7
Carlos Escutia <carlosescutia@gmail.com>
2023-10-20 15:03:26 -0600

    Se implementan vacaciones y justificantes.

---
+++ application/controllers/Justificantes.php
@@ -0,0 +1,193 @@
+<?php
+class Justificantes extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('accesos_sistema_model');
+        $this->load->model('opciones_sistema_model');
+        $this->load->model('bitacora_model');
+        $this->load->model('parametros_sistema_model');
+        $this->load->model('justificantes_model');
+    }
+
+    public function get_userdata()
+    {
+        $cve_rol = $this->session->userdata('cve_rol');
+        $data['cve_usuario'] = $this->session->userdata('cve_usuario');
+        $data['cve_organizacion'] = $this->session->userdata('cve_organizacion');
+        $data['nom_organizacion'] = $this->session->userdata('nom_organizacion');
+        $data['cve_rol'] = $cve_rol;
+        $data['nom_usuario'] = $this->session->userdata('nom_usuario');
+        $data['error'] = $this->session->flashdata('error');
+        $data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+        $data['opciones_sistema'] = $this->opciones_sistema_model->get_opciones_sistema();
+        return $data;
+    }
+
+    public function get_system_params()
+    {
+        $data['nom_sitio_corto'] = $this->parametros_sistema_model->get_parametro_sistema_nom('nom_sitio_corto');
+        $data['nom_sitio_largo'] = $this->parametros_sistema_model->get_parametro_sistema_nom('nom_sitio_largo');
+        $data['nom_org_sitio'] = $this->parametros_sistema_model->get_parametro_sistema_nom('nom_org_sitio');
+        $data['anio_org_sitio'] = $this->parametros_sistema_model->get_parametro_sistema_nom('anio_org_sitio');
+        $data['tel_org_sitio'] = $this->parametros_sistema_model->get_parametro_sistema_nom('tel_org_sitio');
+        $data['correo_org_sitio'] = $this->parametros_sistema_model->get_parametro_sistema_nom('correo_org_sitio');
+        $data['logo_org_sitio'] = $this->parametros_sistema_model->get_parametro_sistema_nom('logo_org_sitio');
+        return $data;
+    }
+
+    public function detalle($cve_justificante)
+    {
+        if ($this->session->userdata('logueado')) {
+            $data = [];
+            $data += $this->get_userdata();
+            $data += $this->get_system_params();
+
+            $data['justificante'] = $this->justificantes_model->get_justificante($cve_justificante);
+
+            $this->load->view('templates/admheader', $data);
+            $this->load->view('justificantes/detalle', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            redirect('admin/login');
+        }
+    }
+    
+    public function nueva_vacacion($cve_empleado)
+    {
+        if ($this->session->userdata('logueado')) {
+            $data = [];
+            $data += $this->get_userdata();
+            $data += $this->get_system_params();
+
+            if ($data['cve_rol'] != 'adm') {
+                redirect('admin');
+            }
+
+            $data['cve_empleado'] = $cve_empleado ;
+
+            $this->load->view('templates/admheader', $data);
+            $this->load->view('justificantes/nueva_vacacion', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            redirect('admin/login');
+        }
+    }
+
+    public function nuevo_justificante($cve_empleado, $fecha)
+    {
+        if ($this->session->userdata('logueado')) {
+            $data = [];
+            $data += $this->get_userdata();
+            $data += $this->get_system_params();
+
+            if ($data['cve_rol'] != 'adm') {
+                redirect('admin');
+            }
+
+            $data['cve_empleado'] = $cve_empleado ;
+            $data['fecha'] = $fecha ;
+
+            $this->load->view('templates/admheader', $data);
+            $this->load->view('justificantes/nuevo_justificante', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            redirect('admin/login');
+        }
+    }
+
+    public function guardar($cve_justificante=null)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            $nuevo_justificante = is_null($cve_justificante);
+
+            $justificante = $this->input->post();
+            if ($justificante) {
+
+                if ($cve_justificante) {
+                    $accion = 'modificó';
+                } else {
+                    $accion = 'agregó';
+                }
+
+                // guardado
+                $data = array(
+                    'cve_empleado' => $justificante['cve_empleado'],
+                    'fecha' => $justificante['fecha'],
+                    'tipo' => $justificante['tipo'],
+                    'detalle' => $justificante['detalle'],
+                    'documento' => $justificante['documento'],
+                );
+                $cve_justificante = $this->justificantes_model->guardar($data, $cve_justificante);
+                
+                // registro en bitacora
+				$separador = ' -> ';
+				$usuario = $this->session->userdata('usuario');
+				$nom_usuario = $this->session->userdata('nom_usuario');
+				$nom_organizacion = $this->session->userdata('nom_organizacion');
+				$entidad = 'justificantes';
+                $valor = $cve_justificante . " " . $justificante['fecha'] . " " . $justificante['tipo'] ;
+				$data = array(
+					'fecha' => date("Y-m-d"),
+					'hora' => date("H:i"),
+					'origen' => $_SERVER['REMOTE_ADDR'],
+					'usuario' => $usuario,
+					'nom_usuario' => $nom_usuario,
+					'nom_organizacion' => $nom_organizacion,
+					'accion' => $accion,
+					'entidad' => $entidad,
+					'valor' => $valor
+				);
+				$this->bitacora_model->guardar($data);
+
+            }
+
+            redirect('admin/empleados_detalle/' . $justificante['cve_empleado']);
+
+        } else {
+            redirect('admin/login');
+        }
+    }
+
+    public function eliminar($cve_justificante)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            // registro en bitacora
+            $justificante = $this->justificantes_model->get_justificante($cve_justificante);
+            $separador = ' -> ';
+            $usuario = $this->session->userdata('usuario');
+            $nom_usuario = $this->session->userdata('nom_usuario');
+            $nom_organizacion = $this->session->userdata('nom_organizacion');
+            $accion = 'eliminó';
+            $entidad = 'justificantes';
+            $valor = $justificante['cve_empleado'] . " " . $justificante['fecha'] . " " . $justificante['tipo'] ;
+
+            $data = array(
+                'fecha' => date("Y-m-d"),
+                'hora' => date("H:i"),
+                'origen' => $_SERVER['REMOTE_ADDR'],
+                'usuario' => $usuario,
+                'nom_usuario' => $nom_usuario,
+                'nom_organizacion' => $nom_organizacion,
+                'accion' => $accion,
+                'entidad' => $entidad,
+                'valor' => $valor
+            );
+            $this->bitacora_model->guardar($data);
+
+            // eliminado
+            $this->justificantes_model->eliminar($cve_justificante);
+
+            redirect('admin/empleados_detalle/' . $justificante['cve_empleado']);
+
+        } else {
+            redirect('admin/login');
+        }
+    }
+
+}
+
--- application/views/admin/empleados_detalle.php
+++ application/views/admin/empleados_detalle.php
@@ -3,7 +3,7 @@
     <div class="col-md-12 mb-3 pb-2 pt-3 border-bottom">
         <div class="row">
             <div class="col-md-4">
-                <h3>Incidentes de <?=$empleado['nom_empleado'] ?></h3>
+                <h3><?=$empleado['nom_empleado'] ?></h3>
             </div>
             <div class="col-sm-4 text-left">
                 <?php include 'selector_mes.php' ?>
@@ -13,40 +13,89 @@
 
     <div class="card mt-0 mb-3 border-0">
         <div class="card-body">
-            <div class="col-sm-6 alternate-color">
-                <div class="row">
-                    <div class="col-sm-3 align-self-center">
-                        <p class="fw-bold">Fecha</p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="row">
+                        <div class="col-sm-3 align-self-center">
+                            <p class="fw-bold">Fecha</p>
+                        </div>
+                        <div class="col-sm-2 align-self-center">
+                            <p class="fw-bold">Entrada</p>
+                        </div>
+                        <div class="col-sm-2 align-self-center">
+                            <p class="fw-bold">Salida</p>
+                        </div>
+                        <div class="col-sm-4 align-self-center">
+                            <p class="fw-bold">Incidente</p>
+                        </div>
                     </div>
-                    <div class="col-sm-2 align-self-center">
-                        <p class="fw-bold">Entrada</p>
+                    <?php foreach ($incidentes_empleado as $incidentes_empleado_item) { ?>
+                    <div class="row alternate-color">
+                        <div class="col-sm-3 align-self-center">
+                            <p><?= date('d/m/Y', strtotime($incidentes_empleado_item['fecha'])) ?></p>
+                        </div>
+                        <div class="col-sm-2 align-self-center">
+                            <p><?= $incidentes_empleado_item['hora_entrada'] ?></p>
+                        </div>
+                        <div class="col-sm-2 align-self-center">
+                            <p><?= $incidentes_empleado_item['hora_salida'] ?></p>
+                        </div>
+                        <div class="col-sm-4 align-self-center">
+                            <p><a href="<?=base_url()?>justificantes/nuevo_justificante/<?=$incidentes_empleado_item['cve_empleado']?>/<?=$incidentes_empleado_item['fecha']?>"><?= $incidentes_empleado_item['incidente'] ?></a></p>
+                        </div>
                     </div>
-                    <div class="col-sm-2 align-self-center">
-                        <p class="fw-bold">Salida</p>
+                    <?php } ?>
+                </div>
+                <div class="col-sm-4 offset-sm-1 offset-sm-1 offset-sm-1">
+                    <div class="card mb-5">
+                        <div class="card-header card-sistema text-center">
+                            <strong>Vacaciones</strong>
+                        </div>
+                        <div class="card-body">
+                            <?php foreach ($vacaciones_empleado as $vacaciones_empleado_item) { ?>
+                            <div class="row alternate-color">
+                                <div class="col-sm-5 offset-sm-1">
+                                    <p><a href="<?=base_url()?>justificantes/detalle/<?=$vacaciones_empleado_item['cve_justificante']?>"><?= date('d/m/Y', strtotime($vacaciones_empleado_item['fecha'])) ?></a></p>
+                                </div>
+                                <div class="col-sm-1">
+                                    <?php 
+                                    $item_eliminar = date('d/m/Y', strtotime($vacaciones_empleado_item['fecha'])) . ' - ' . $vacaciones_empleado_item['tipo'];
+                                    $url = base_url() . "justificantes/eliminar/". $vacaciones_empleado_item['cve_justificante']; 
+                                    ?>
+                                    <p><a href="#dlg_borrar" data-bs-toggle="modal" onclick="pass_data('<?=$item_eliminar?>', '<?=$url?>')" ><i class="bi bi-x-circle boton-eliminar" ></i>
+                                    </a></p>
+                                </div>
+                            </div>
+                            <?php } ?>
+                        </div>
+                        <div class="card-footer text-end">
+                            <a class="btn btn-primary btn-sm" href="<?=base_url()?>justificantes/nueva_vacacion/<?=$incidentes_empleado_item['cve_empleado']?>">Agregar</a>
+                        </div>
                     </div>
-                    <div class="col-sm-4 align-self-center">
-                        <p class="fw-bold">Incidente</p>
+                    <div class="card mb-3">
+                        <div class="card-header card-sistema text-center">
+                            <strong>Justificantes</strong>
+                        </div>
+                        <div class="card-body">
+                            <?php foreach ($justificantes_empleado as $justificantes_empleado_item) { ?>
+                            <div class="row alternate-color">
+                                <div class="col-sm-5 offset-sm-1">
+                                    <p><a href="<?=base_url()?>justificantes/detalle/<?=$justificantes_empleado_item['cve_justificante']?>"><?= date('d/m/Y', strtotime($justificantes_empleado_item['fecha'])) ?> - <?= $justificantes_empleado_item['tipo'] ?></a></p>
+                                </div>
+                                <div class="col-sm-1">
+                                    <?php 
+                                    $item_eliminar = date('d/m/Y', strtotime($justificantes_empleado_item['fecha'])) . ' - ' . $justificantes_empleado_item['tipo'];
+                                    $url = base_url() . "justificantes/eliminar/". $justificantes_empleado_item['cve_justificante']; 
+                                    ?>
+                                    <p><a href="#dlg_borrar" data-bs-toggle="modal" onclick="pass_data('<?=$item_eliminar?>', '<?=$url?>')" ><i class="bi bi-x-circle boton-eliminar" ></i>
+                                    </a></p>
+                                </div>
+                            </div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
             </div>
-            <?php foreach ($incidentes_empleado as $incidentes_empleado_item) { ?>
-            <div class="col-sm-6 alternate-color">
-                <div class="row">
-                    <div class="col-sm-3 align-self-center">
-                        <p><?= date('d/m/Y', strtotime($incidentes_empleado_item['fecha'])) ?></p>
-                    </div>
-                    <div class="col-sm-2 align-self-center">
-                        <p><?= $incidentes_empleado_item['hora_entrada'] ?></p>
-                    </div>
-                    <div class="col-sm-2 align-self-center">
-                        <p><?= $incidentes_empleado_item['hora_salida'] ?></p>
-                    </div>
-                    <div class="col-sm-4 align-self-center">
-                        <p><?= $incidentes_empleado_item['incidente'] ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php } ?>
         </div>
     </div>
 
